8432e340b219faaaa70616e42a037cd5605e83cf
ascooper57 <cooper@pobox.com>
2019-08-10 14:57:58 -0700

    Mac utilities

---
+++ example/stop_postgres.sh
@@ -0,0 +1,2 @@
+brew services stop postgres
+rm -f /usr/local/var/postgres/postmaster.pid
---
+++ example/update_awsebcli.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+brew config
+#xcode-select --install
+brew update
+brew upgrade
+pip3 install --upgrade pip
+pip3 install --upgrade awsebcli
+pip3 install --upgrade pip setuptools
+npm install -g npm to update 
+npm install -g npm
+npm install -g nwb
