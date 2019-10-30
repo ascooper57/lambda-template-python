b88dbf4466e3ce95274e2bb50cbffebccde7c4af
ascooper57 <cooper@pobox.com>
2019-10-30 11:12:38 -0700

    Cleaning up bash syntax

--- example/merge_after_codegen.sh
+++ example/merge_after_codegen.sh
@@ -2,21 +2,21 @@
 
 CWD=$(pwd)
 DOWNLOADS=~/Downloads
-PRAKTIKOS_HOME="${CWD}/../"
+PROJECT_ROOT="${CWD}/../"
 
 echo Prepare "${DOWNLOADS}" folder
 pushd "${DOWNLOADS}" || exit 1
-rm -rf "${DOWNLOADS}"/praktikos-template-python > /dev/null
-mkdir -p "${DOWNLOADS}"/praktikos-template-python
+rm -rf "${DOWNLOADS}/praktikos-template-python" > /dev/null
+mkdir -p "${DOWNLOADS}/praktikos-template-python"
 
 echo "Unzip generated files"
-cd praktikos-template-python || exit 1
-unzip ../praktikos-template-python.zip
+cd "${DOWNLOADS}/praktikos-template-python" || exit 1
+unzip "${DOWNLOADS}/praktikos-template-python.zip"
 cd "${DOWNLOADS}" || exit 1
-ditto -V praktikos-template-python ${PRAKTIKOS_HOME}
+ditto -V "${DOWNLOADS}/praktikos-template-python" "${PROJECT_ROOT}"
 
 echo "Merge and run automated tests on generated code changes"
-cd "${PRAKTIKOS_HOME}" || exit 1
+cd "${PROJECT_ROOT}" || exit 1
 cp -pf api/rdb/model/model_list.py.MERGE api/rdb/model/model_list.py
 cp -pf "${CWD}/event.json" api/lambda_functions/LambdaApiGenerated/event.json
 
